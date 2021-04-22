47c5db49753751421570aed4c6d4032eff0d6f52
Gleb Markin <gleb.markin@banuba.com>
2021-04-22 13:37:32 +0300

    1.0.15.native-script

---
+++ VideoEditor.framework/Headers/VideoEditor.h
@@ -0,0 +1,19 @@
+//
+//  VideoEditor.h
+//  VideoEditor
+//
+//  Created by Alexey Ereschenko on 02/05/2019.
+//  Copyright © 2019 Banuba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VideoEditor.
+FOUNDATION_EXPORT double VideoEditorVersionNumber;
+
+//! Project version string for VideoEditor.
+FOUNDATION_EXPORT const unsigned char VideoEditorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VideoEditor/PublicHeader.h>
+
+
